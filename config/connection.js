fa13b18266813c695b877cc4eb043dd4e873aa7f
unknown <nathan.c.ginn@gmail.com>
2020-03-01 16:52:19 -0800

    sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var Sequelize = require("sequelize");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "123456789",
-  database: "burger_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  console.log ("going local");
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
@@ -18,5 +26,21 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+var sequelize = new Sequelize(
+  "burgers_db", 
+  "root", 
+  "123456789", 
+  {
+      host: "localhost",
+      port: 3306,
+      dialect: "mysql",
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      }
+  }
+);
+
 // Export connection for our ORM to use.
-module.exports = connection;
+module.exports = sequelize;
