dc48fe91e647c08956ac64e203239e7824ff3a58
unknown <nathan.c.ginn@gmail.com>
2020-03-01 16:29:43 -0800

    first push

---
+++ controllers/burgersController.js
@@ -0,0 +1,59 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+  burger.findAll({ raw: true }).then(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.create({
+    name: req.body.name,
+    devoured: req.body.devoured
+  }).then( function(result) {
+    res.json(result.toJSON()); 
+  });
+});
+
+router.put("/api/burger8000/:id", function(req, res) {
+  burger.update(
+    {
+      devoured: req.body.devoured
+    },
+    {
+      where :{
+        id: req.params.id
+      }
+    }
+    ).then(function(result){
+      res.status(200).end();
+    }).catch(function(err){
+      res.status(404).end()
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  
+  burger.destroy({
+    where:{
+      id: Number(req.params.id)
+    }
+  }).then(function(result){
+    res.status(200).end();
+  }).catch(function(err){
+    res.status(404).end()
+  })
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('beesechurger');
+INSERT INTO burgers (name) VALUES ('cheeseborger');
+INSERT INTO burgers (name, devoured) VALUES ('mushy berger', true);
+INSERT INTO burgers (name, devoured) VALUES ('bargar', true);
+INSERT INTO burgers (name, devoured) VALUES ('hot dog', true);
+INSERT INTO burgers (name) VALUES ('double bacon supreme');
